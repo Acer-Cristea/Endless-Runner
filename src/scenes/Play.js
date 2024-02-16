66ed277605dce44fdcfc2a0df624284cd0f3d34d
Acer Cristea <acristea@ucsc.edu>
2024-02-15 22:40:13 -0800

    Added increase challenge after 30 seconds

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -31,6 +31,13 @@ class Play extends Phaser.Scene {
         this.rock2 = this.physics.add.sprite(Phaser.Math.Between(0, 640),0, "rock")
         this.rock2.body.setImmovable(true)
 
+        this.rock4 = this.physics.add.sprite(Phaser.Math.Between(0, 640),0, "rock")
+        this.rock4.body.setImmovable(true)
+
+        this.rock3 = this.physics.add.sprite(Phaser.Math.Between(0, 640),0, "rock")
+        this.rock3.body.setImmovable(true)
+        this.rock3.setVisible(false)
+        this.rock3.setActive(false)
 
         this.crab1 = this.physics.add.sprite(width, 650, "crab")
         this.crab1.setVelocityX(-100)
@@ -43,10 +50,23 @@ class Play extends Phaser.Scene {
         this.crab2.body.setCircle(this.crab2.width/2)
         this.crab2.body.setImmovable(true)
 
-        this.crab3 = this.physics.add.sprite(width, Phaser.Math.Between(500, 810), "crab")
+        this.crab3 = this.physics.add.sprite(width, Phaser.Math.Between(100, 300), "crab")
         this.crab3.setVelocityX(-200)
         this.crab3.body.setCircle(this.crab3.width/2)
         this.crab3.body.setImmovable(true)
+  
+        this.crab4 = this.physics.add.sprite(width, Phaser.Math.Between(300, 610), "crab")
+        this.crab4.setVelocityX(-250)
+        this.crab4.body.setCircle(this.crab3.width/2)
+        this.crab4.body.setImmovable(true)
+        this.crab4.setVisible(false)
+        this.crab4.setActive(false)
+
+        this.crab5 = this.physics.add.sprite(width, Phaser.Math.Between(100, 300), "crab")
+        this.crab5.setVelocityX(-225)
+        this.crab5.body.setCircle(this.crab1.width/2)
+        this.crab5.body.setImmovable(true)
+
 
 
         this.soccerBall = this.physics.add.sprite(width/2, height-100,"soccerBall")
@@ -121,6 +141,20 @@ class Play extends Phaser.Scene {
 
         })    
 
+        this.physics.add.collider(this.rock4, this.player, (rock, player) => {
+
+            this.health -= 1
+
+        })  
+
+        this.rock3collider = this.physics.add.collider(this.rock3, this.player, (rock, player) => {
+
+            this.health -= 1
+
+        })  
+
+        this.rock3collider.active = false
+
 
         this.physics.add.collider(this.soccerBall, this.crab1, (soccerBall, crab) => {
 
@@ -180,6 +214,27 @@ class Play extends Phaser.Scene {
         })
 
 
+        this.crab4collider = this.physics.add.collider(this.soccerBall, this.crab4, (soccerBall, crab) => {
+
+            this.score+=25
+
+            let shotDirectionX = crab.x <= this.soccerBall.x ? 1 : -1 
+            let shotDirectionY = crab.y <= this.soccerBall.y ? -1 : 1 
+
+            this.soccerBall.body.setVelocityX((this.WEAK_SHOT_X_VELOCITY)*shotDirectionX)
+            this.soccerBall.body.setVelocityY((this.WEAK_SHOT_Y_VELOCITY)*shotDirectionY)
+            let hitSounds = ["hit1", "hit2"]
+
+            let randomIndex = Math.floor(Math.random()*2)
+            let randomHit = hitSounds[randomIndex]
+      
+            this.sound.play(randomHit)
+
+        })
+
+        this.crab4collider.active = false
+
+
 
     }
 
@@ -207,6 +262,8 @@ class Play extends Phaser.Scene {
 
         this.rock1.y += 6
         this.rock2.y += 4
+        this.rock3.y += 5
+        this.rock4.y += 5.5
 
 
         if (this.crab1.x <= 0) {
@@ -224,6 +281,16 @@ class Play extends Phaser.Scene {
             this.crab3.y = Phaser.Math.Between(500, 730)
         }
 
+        if (this.crab4.x <= 0) {
+            this.crab4.x = width
+            this.crab4.y = Phaser.Math.Between(300, 730)
+        }
+
+        if (this.crab5.x <= 0) {
+            this.crab5.x = width
+            this.crab5.y = Phaser.Math.Between(100, 300)
+        }
+
         if (this.rock1.y >= 870) {
             this.rock1.y = 0
             this.rock1.x = Phaser.Math.Between(0, 640)
@@ -232,6 +299,28 @@ class Play extends Phaser.Scene {
         if (this.rock2.y >= 870) {
             this.rock2.y = 0
             this.rock2.x = Phaser.Math.Between(0, 640)
+        }
+
+        if (this.rock3.y >= 870) {
+            this.rock3.y = 0
+            this.rock3.x = Phaser.Math.Between(0, 640)
+        } 
+
+        if (this.rock4.y >= 870) {
+            this.rock4.y = 0
+            this.rock4.x = Phaser.Math.Between(0, 640)
+        } 
+        
+        if(this.time.now >= 30000){
+            this.rock3collider.active = true
+            this.rock3.setVisible(true)
+            this.rock3.setActive(true)
+        }
+
+        if(this.time.now >= 30000){
+            this.crab4collider.active = true
+            this.crab4.setVisible(true)
+            this.crab4.setActive(true)
         }
 
 
